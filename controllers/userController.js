ccc936182954355998e789b57ef7d387f8dfa9b1
Marko <horvat.marko2212@gmail.com>
2023-02-13 23:29:28 +0100

    login functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,13 @@
 const User = require("../models/User");
 
+exports.login = function (req, res) {
+  let user = new User(req.body);
+  user
+    .login()
+    .then((result) => res.send(result))
+    .catch((err) => res.send(err));
+};
+
 exports.register = function (req, res) {
   let user = new User(req.body);
   user.register();
--- models/User.js
+++ models/User.js
@@ -54,7 +54,25 @@ User.prototype.validate = function () {
   }
 };
 
-User.prototype.register = async function () {
+User.prototype.login = function () {
+  return new Promise((resolve, reject) => {
+    this.cleanUp();
+    usersCollection
+      .findOne({
+        username: this.data.username,
+      })
+      .then((attemptedUser) => {
+        if (attemptedUser && attemptedUser.password === this.data.password) {
+          resolve("Congrats!!!");
+        } else {
+          reject("Invalid username / password");
+        }
+      })
+      .catch(() => reject("Please try again later."));
+  });
+};
+
+User.prototype.register = function () {
   // validate user data
   this.cleanUp();
   this.validate();
