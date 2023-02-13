396f566b70e826dc881b763daeaaadba728fdd60
Marko <horvat.marko2212@gmail.com>
2023-02-13 16:14:49 +0100

    cleaning up and validating registration form

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,11 @@ const User = require("../models/User");
 exports.register = function (req, res) {
   let user = new User(req.body);
   user.register();
-  res.send("Tnx for trying to registrate!");
+  if (user.errors.length) {
+    res.send(user.errors);
+  } else {
+    res.send("Congrats, there are no errors.");
+  }
 };
 
 exports.home = function (req, res) {
