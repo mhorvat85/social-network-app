4696cee0f1fece319c22a1ff1e4dc8e243924e5c
Marko <horvat.marko2212@gmail.com>
2023-02-14 16:58:55 +0100

    creating session

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,10 @@ exports.login = function (req, res) {
   let user = new User(req.body);
   user
     .login()
-    .then((result) => res.send(result))
+    .then((result) => {
+      req.session.user = { username: user.data.username };
+      res.send(result);
+    })
     .catch((err) => res.send(err));
 };
 
@@ -19,5 +22,9 @@ exports.register = function (req, res) {
 };
 
 exports.home = function (req, res) {
-  res.render("home-guest");
+  if (req.session.user) {
+    res.send("Welcome to the actual application!");
+  } else {
+    res.render("home-guest");
+  }
 };
